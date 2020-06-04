fbaf41d923c038678c5be1f39eeee947f6e5ea62
sukanyaspai <sukanya.pai1011@gmail.com>
2020-06-04 17:54:37 +0530

    Spring Controller and Spring App

---
+++ spring-boot/src/main/java/com/example/springboot/SpringBootAppApplication.java
@@ -0,0 +1,34 @@
+package com.example.springboot;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class SpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("****************************************************");
+			System.out.println("Beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+			System.out.println("****************************************************");
+
+		};
+	}
+
+}
